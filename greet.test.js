b6a7075fc889fdc9bc7015999d64a36011bb721d
M-Tumelo <tumelomonyemangene99@gmail.com>
2021-08-01 12:39:53 +0200

    modified greet and yearsAgo tests

--- greet.test.js
+++ greet.test.js
@@ -6,8 +6,11 @@ describe('This test is testing the greet function' , function(){
     it('should show you Hello Sima' , function(){
         assert.deepEqual("Hello, Sima",greet("Sima"));
     });
-    it('should not equal to Hello Sima' , function(){
+    it('should not equal to Hello Sima when Dima is inputted as a parameter' , function(){
         assert.notEqual("Hello, Sima",greet("Dima"));
+    });
+    it('should not equal to Hello Dima when Sima is inputted as a parameter' , function(){
+        assert.notEqual("Hello, Dima",greet("Sima"));
     });
 });
 
--- yearsAgo.test.js
+++ yearsAgo.test.js
@@ -1,9 +1,9 @@
 //TestCase for yearsAgo function
 describe('This test is testing the yearsAgo function' , function(){
-    it('should show the number of numberplates belonging to Paarl (CJ)' , function(){
-        assert.equal(3, countAllPaarl('CJ 345 123, CJ 2345, CL 123-546, CK 345, CJ 123'));
+    it('should return the number of years passed since 1976' , function(){
+        assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976));
     });
-    it('should show the number of numberplates belonging to Paarl (CJ)' , function(){
-        assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));    
+    it('should return the number of years passed since 2000' , function(){
+        assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000));    
     });
 });
